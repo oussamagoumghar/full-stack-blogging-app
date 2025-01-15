196e79fb6c70be0f972bcfa355f2bbad85d74755
oussama.goumghar@nestle.com <oussama.goumghar@nestle.com>
2025-01-15 16:23:18 +0100

    adjust the availability zones

--- EKS_Terraform/main.tf
+++ EKS_Terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-2"
+  region = "eu-north-1"
 }
 
 resource "aws_vpc" "devopsshack_vpc" {
@@ -14,7 +14,7 @@ resource "aws_subnet" "devopsshack_subnet" {
   count = 2
   vpc_id                  = aws_vpc.devopsshack_vpc.id
   cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
-  availability_zone       = element(["eu-west-2a", "eu-west-2c"], count.index)
+  availability_zone       = element(["eu-north-1a", "eu-north-1c"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
@@ -108,7 +108,7 @@ resource "aws_eks_node_group" "devopsshack" {
     min_size     = 3
   }
 
-  instance_types = ["t2.large"]
+  instance_types = ["t3.large"]
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
